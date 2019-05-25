238260e62ef81f585fa83fc645788496c5f03cde
sparshsharma13 <sparshsharma13@yahoo.com>
2019-05-25 00:26:07 -0700

    first commit

---
+++ flight.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import pandas as pd
+import numpy as np
+df=pd.read_csv("flightdelays.csv")
+data=df[df['Origin']=='SFO']['ArrDelay'].head(3)
+data.to_csv("first3sfo.csv")
+first3sfo=pd.read_csv("first3sfo.csv" , header=None)[1]
+print(first3sfo)
+dest=df['Dest'].value_counts().head(3)
+print(dest)
---
+++ flight.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+awk -F "\"*,\"*" '{ if ($17 == "SFO" )  print $15}' flightdelays.csv | head -n 3 > first3sfo.csv 
+csvlook -H first3sfo.csv 
+awk -F "\"*,\"*" '{  print $18 }' flightdelays.csv | sort | uniq -c | sort -nr | head -n 3 | csvlook -H
+
